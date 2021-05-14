6c3157a011770b5567c30e7140a2329210465989
ufoo68 <mitarou.yuutarou@gmail.com>
2021-05-14 17:45:09 +0900

    Copy meeting page and remove css

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,8 @@
-import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
-import './App.css'
+import { Meeting } from './features/meeting/Meeting'
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Counter />
-      <span>
-        <span>Learn </span>
-        <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-          React
-        </a>
-        <span>, </span>
-        <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-          Redux
-        </a>
-        <span>, </span>
-        <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-          Redux Toolkit
-        </a>
-        <span> and </span>
-        <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-          React Redux
-        </a>
-      </span>
-    </header>
+  <div>
+    <Meeting />
   </div>
 )
 
---
+++ app/src/features/meeting/Meeting.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import Webcam from 'react-webcam'
+import { Document, pdfjs, Page } from 'react-pdf'
+import { makeStyles } from '@material-ui/core/styles'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  presenter: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  slide: {
+    width: 650,
+  },
+  slideView: {
+    height: 450,
+  },
+  slideButton: {
+    height: 50,
+    textAlign: 'center',
+  },
+  webcam: {
+    height: 200,
+    alignSelf: 'flex-end',
+  },
+  timer: {
+    textAlign: 'center',
+    fontSize: '50px',
+  },
+})
+
+export const Meeting = () => {
+  const classes = useStyles()
+  const [base64, setBase64] = useState<string>('')
+  const [numPages, setNumPages] = useState<number>(1)
+  const [pages, setPages] = useState<number>(1)
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.presenter}>
+        <div className={classes.slide}>
+          {base64 ? (
+            <Document file={base64} onLoadSuccess={pdf => setNumPages(pdf.numPages)} className={classes.slideView}>
+              <Page pageNumber={pages} />
+            </Document>
+          ) : (
+            <input
+              type="file"
+              className={classes.slideView}
+              onChange={e => {
+                const file = e!.target!.files![0]
+                const reader = new FileReader()
+                reader.readAsDataURL(file)
+                reader.onload = () => {
+                  setBase64(reader.result as string)
+                }
+              }}
+            />
+          )}
+          <div className={classes.slideButton}>
+            <button disabled={pages <= 1} onClick={() => setPages(pages - 1)}>
+              Prev
+            </button>
+            <button disabled={pages >= numPages || !numPages} onClick={() => setPages(pages + 1)}>
+              Next
+            </button>
+          </div>
+        </div>
+        <Webcam className={classes.webcam} />
+      </div>
+    </div>
+  )
+}
