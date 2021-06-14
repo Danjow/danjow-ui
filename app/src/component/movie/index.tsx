5365712e5514a2cc7a182cf9cf8d02f16f23edf5
ufoo68 <mitarou.yuutarou@gmail.com>
2021-06-14 21:35:17 +0900

    Implement slide and movie mode

---
+++ app/src/component/movie/index.tsx
@@ -0,0 +1,3 @@
+import ReactPlayer from 'react-player'
+
+export const Movie = () => <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
--- app/src/features/meeting/Meeting.tsx
+++ app/src/features/meeting/Meeting.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import Webcam from 'react-webcam'
-import { Document, pdfjs, Page } from 'react-pdf'
 import { makeStyles } from '@material-ui/core/styles'
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import { Slide } from '../../component/slide'
+import { Movie } from '../../component/movie'
 
 const useStyles = makeStyles({
   container: {
@@ -15,64 +14,37 @@ const useStyles = makeStyles({
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-  slide: {
-    width: 650,
-  },
-  slideView: {
-    height: 450,
-  },
-  slideButton: {
-    height: 50,
-    textAlign: 'center',
-  },
   webcam: {
     height: 200,
     alignSelf: 'flex-end',
   },
-  timer: {
-    textAlign: 'center',
-    fontSize: '50px',
-  },
 })
+
+type Mode = 'slide' | 'movie'
 
 export const Meeting = () => {
   const classes = useStyles()
-  const [base64, setBase64] = useState<string>('')
-  const [numPages, setNumPages] = useState<number>(1)
-  const [pages, setPages] = useState<number>(1)
+  const [mode, setMode] = useState<Mode>('slide')
+  const sharedView = () => {
+    switch (mode) {
+      case 'slide':
+        return <Slide />
+      case 'movie':
+        return <Movie />
+      default:
+        return <Slide />
+    }
+  }
 
   return (
     <div className={classes.container}>
       <div className={classes.presenter}>
-        <div className={classes.slide}>
-          {base64 ? (
-            <Document file={base64} onLoadSuccess={pdf => setNumPages(pdf.numPages)} className={classes.slideView}>
-              <Page pageNumber={pages} />
-            </Document>
-          ) : (
-            <input
-              type="file"
-              className={classes.slideView}
-              onChange={e => {
-                const file = e!.target!.files![0]
-                const reader = new FileReader()
-                reader.readAsDataURL(file)
-                reader.onload = () => {
-                  setBase64(reader.result as string)
-                }
-              }}
-            />
-          )}
-          <div className={classes.slideButton}>
-            <button disabled={pages <= 1} onClick={() => setPages(pages - 1)}>
-              Prev
-            </button>
-            <button disabled={pages >= numPages || !numPages} onClick={() => setPages(pages + 1)}>
-              Next
-            </button>
-          </div>
-        </div>
+        {sharedView()}
         <Webcam className={classes.webcam} />
+      </div>
+      <div>
+        <button onClick={() => setMode('slide')}>slide</button>
+        <button onClick={() => setMode('movie')}>movie</button>
       </div>
     </div>
   )
